d45ae9c7ef30bd0674ba0c2a76f2e91ed6b8c28b
School47 <School47@gmail.com>
2023-03-23 13:55:17 +0500

    task3

--- task01/Program.cs
+++ task01/Program.cs
@@ -1,12 +1,20 @@
-﻿        Console.Write("Введите первое число: ");
-        int a = Convert.ToInt32((Console.ReadLine()));
-        Console.Write("Введите второе число: ");
-        int b = Convert.ToInt32((Console.ReadLine()));
-        if (a == b*b)
-        {
-            Console.WriteLine($"{a} является квадратом {b}. УРА!");
-        }
-        else
-        {
-            Console.WriteLine($"{a} не является квадратом {b}. Обидно");
-        }
+﻿// 1. Напишите программу на C#, которая на вход принимает два
+// числа и проверяет, является ли первое число квадратом
+// второго.
+// a = 25, b = 5 -> да
+// a = 2, b = 10 -> нет
+// a = 9, b = -3 -> да
+// a = -3 b = 9 -> нет
+
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32((Console.ReadLine()));
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32((Console.ReadLine()));
+if (a == b * b)
+{
+    Console.WriteLine($"{a} является квадратом {b}. УРА!");
+}
+else
+{
+    Console.WriteLine($"{a} не является квадратом {b}. Обидно");
+}
